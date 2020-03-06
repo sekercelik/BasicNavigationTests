10fce172fbb70615d4f0cbf256277fe367fe99d7
sekercelik <sceliksidika@gmail.com>
2020-03-06 12:50:40 -0500

    added test methods for browsers

---
+++ src/test/java/com/cbt/utilities/TitleVerification.java
@@ -0,0 +1,26 @@
+package com.cbt.utilities;
+import org.openqa.selenium.WebDriver;
+
+import java.util.*;
+
+public class TitleVerification {
+    public static void main(String[] args) {
+
+        List<String> urls = Arrays.asList("http://practice.cybertekschool.com/",
+                "http://practice.cybertekschool.com/dropdown",
+                "http://practice.cybertekschool.com/login");
+
+        WebDriver driver= BrowserFactory.getDriver("chrome");
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
---
+++ src/test/java/com/cbt/utilities/TitleVerification2.java
@@ -0,0 +1,17 @@
+package com.cbt.utilities;
+import java.util.*;
+
+
+public class TitleVerification2 {
+    public static void main(String[] args) {
+
+        List<String> urls = Arrays.asList("https://lulugandgeorgia.com",
+                "https://wayfair.com/",
+                "https://walmart.com",
+                 "https://westelm.com/");
+
+
+
+
+    }
+}
