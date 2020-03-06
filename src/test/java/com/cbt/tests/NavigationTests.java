030b44a363328ce511a4dea833c16a6708760973
sekercelik <sceliksidika@gmail.com>
2020-03-06 02:45:12 -0500

    added test methods for browsers

---
+++ src/test/java/com/cbt/tests/NavigationTests.java
@@ -0,0 +1,61 @@
+package com.cbt.tests;
+
+import com.cbt.utilities.BrowserFactory;
+import com.cbt.utilities.StringUtility;
+import org.openqa.selenium.WebDriver;
+
+public class NavigationTests {
+    public static void main(String[] args) {
+    chromeTest();
+   // FirefoxTest();
+   // EdgeTest();
+
+
+    }
+
+public static void chromeTest(){
+
+   WebDriver driver= BrowserFactory.getDriver("chrome");
+    driver.get("https://google.com");
+    String str1= driver.getTitle();
+    driver.navigate().to("https://etsy.com");
+    String str2=driver.getTitle();
+    driver.navigate().back();
+    StringUtility.verifyEquals(driver.getTitle(),str1);
+    driver.navigate().forward();
+    StringUtility.verifyEquals(driver.getTitle(),str2);
+    driver.quit();
+
+}
+
+//*************************************************************************
+
+    public static void FirefoxTest(){
+        WebDriver driver= BrowserFactory.getDriver("firefox");
+        driver.get("https://google.com");
+        String str1= driver.getTitle();
+        driver.navigate().to("https://etsy.com");
+        String str2=driver.getTitle();
+        driver.navigate().back();
+        StringUtility.verifyEquals(driver.getTitle(),str1);
+        driver.navigate().forward();
+        StringUtility.verifyEquals(driver.getTitle(),str2);
+        driver.quit();
+
+    }
+
+    public static void EdgeTest(){
+        WebDriver driver= BrowserFactory.getDriver("edge");
+        driver.get("https://google.com");
+        String str1= driver.getTitle();
+        driver.navigate().to("https://etsy.com");
+        String str2=driver.getTitle();
+        driver.navigate().back();
+        StringUtility.verifyEquals(driver.getTitle(),str1);
+        driver.navigate().forward();
+        StringUtility.verifyEquals(driver.getTitle(),str2);
+        driver.quit();
+    }
+
+
+}
