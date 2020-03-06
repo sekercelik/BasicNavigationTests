efd9d088288553f05f743a2199b51313e98c4f62
sekercelik <sceliksidika@gmail.com>
2020-03-06 02:09:43 -0500

    added methods

--- src/test/java/com/cbt/utilities/StringUtility.java
+++ src/test/java/com/cbt/utilities/StringUtility.java
@@ -2,7 +2,14 @@ package com.cbt.utilities;
 
 public class StringUtility {
 
-
+public static void verifyEquals(String actual, String expected){
+    if(actual.equals(expected)){
+        System.out.println("PASS");
+    }else{
+        System.out.println("FAIL");
+        System.out.println("actual: "+ actual + ", expected: "+ expected);
+    }
+}
 
 
 }
